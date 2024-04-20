3e01e5cccf7f3cc7616c9e9c99db1e5ca5927b80
tyrohellion <evanmarchesani@gmail.com>
2024-04-20 05:47:40 -0400

    fullscreen search dialog and removed cs for now

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let sidebar = document.querySelector('.sidebar');
 let icon = document.querySelector('.sidebar-icon');
+let search = document.querySelector('.fullscreen-search-container');
+let searchButton = document.querySelector('.search-button');
+let closeSearch = document.querySelector('.x-icon');
+
+sidebar.classList.toggle('active');
 
 icon.onclick = function() {
     sidebar.classList.toggle('active');
 };
+
+searchButton.onclick = function() {
+    search.classList.toggle('active');
+};
+
+closeSearch.onclick = function() {
+    search.classList.toggle('active');
+};
+
+
